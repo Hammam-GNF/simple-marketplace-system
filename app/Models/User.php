6576854e4d40506bba98821ca91dc6b64c8ba6c5
Hammam GNF <hammamgonjil@gmail.com>
2026-02-02 21:25:35 +0700

    feat(transaction): add transactions table and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
