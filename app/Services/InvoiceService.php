6984842e5dabcc67edc36a6835973e76c55c95c2
Hammam GNF <hammamgonjil@gmail.com>
2026-02-04 19:31:14 +0700

    feat(invoice): implement invoice PDF generator service

--- app/Services/InvoiceService.php
+++ app/Services/InvoiceService.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 namespace App\Services;
 
@@ -7,12 +7,15 @@ use Barryvdh\DomPDF\Facade\Pdf;
 
 class InvoiceService
 {
+    /**
+     * Create a new class instance.
+     */
     public function generate(Transaction $transaction)
     {
         $transaction->load(['user', 'product']);
 
         return Pdf::loadView('pdf.invoice', [
-            'transaction' => $transaction
+            'transaction' => $transaction,
         ]);
     }
 }
